855589296e83aa0599379b8d979a1662d070a9a7
Nick Paolino <github email address>
2017-12-13 20:25:21 -0500

    Created authReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import authReducer from "./reducers/authReducer.js";
 
 const rootReducer = combineReducers({
-  firstKey: null
+  firstKey: authReducer
 });
 
 export default rootReducer;
