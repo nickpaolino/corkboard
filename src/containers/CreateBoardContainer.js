047129582eefcfc08c9f8c0a0daf851118d9b4bd
Nick Paolino <github email address>
2017-12-16 12:02:15 -0500

    Created state for dropdown values

--- src/containers/CreateBoardContainer.js
+++ src/containers/CreateBoardContainer.js
@@ -1,31 +1,68 @@
 import React, { Component } from "react";
-import { Dropdown } from "semantic-ui-react";
+import { Form, Button, Dropdown, Checkbox } from "semantic-ui-react";
 
 class CreateBoardContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      existingSubject: "",
+      newSubject: "",
+      newSubjectIsDefault: null
+    };
+  }
+  handleChange = e => {
+    let newState;
+
+    if (!!e.target.value) {
+      newState = {
+        newSubject: e.target.value,
+        newSubjectIsDefault: true
+      };
+    } else {
+      newState = {
+        existingSubject: e.target.innerText,
+        newSubjectIsDefault: false
+      };
+    }
+    this.setState(newState);
+  };
   render() {
     const subjectOptions = [
-      { key: 1, value: "Bitcoin", text: "Bitcoin" },
+      { key: "Bitcoin", value: "Bitcoin", text: "Bitcoin" },
       {
-        key: 2,
+        key: "Russia Investigation",
         value: "Russia Investigation",
         text: "Russia Investigation"
       },
       {
-        key: 3,
+        key: "Climate Change",
         value: "Climate Change",
         text: "Climate Change"
       }
     ];
+    console.log(this.state);
     return (
       <div>
         <h2>Create a Board</h2>
-        <label>Subject: </label>
-        <Dropdown
-          placeholder="Subject"
-          search
-          selection
-          options={subjectOptions}
-        />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field inline>
+            <label>Find or Create a Subject: </label>
+            <Dropdown
+              placeholder="Subject"
+              search
+              selection
+              additionPosition="top"
+              allowAdditions
+              options={subjectOptions}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field inline>
+            <Checkbox label="This board is open to the public" defaultChecked />
+          </Form.Field>
+          <Button>Create</Button>
+        </Form>
       </div>
     );
   }
