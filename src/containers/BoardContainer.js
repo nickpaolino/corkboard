4a48620e33369bbb7f9973e05f4405fd07f02da4
Nick Paolino <github email address>
2017-12-17 15:25:16 -0500

    Added BoardDisplayContainer which will hold each board layout

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import withAuth from "../components/hocs/withAuth";
 import { Route, Switch } from "react-router-dom";
 import CreateBoardContainer from "./CreateBoardContainer";
+import BoardDisplayContainer from "./BoardDisplayContainer";
 
 class BoardContainer extends Component {
   render() {
@@ -9,6 +10,12 @@ class BoardContainer extends Component {
       <div>
         <Switch>
           <Route path="/boards/new" component={CreateBoardContainer} />
+          <Route
+            path="/boards/:id"
+            render={props => {
+              return <BoardDisplayContainer boardId={props.match.params.id} />;
+            }}
+          />
         </Switch>
       </div>
     );
---
+++ src/containers/BoardDisplayContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class BoardDisplayContainer extends Component {
+  render() {
+    console.log(this.props.boardId);
+    return <div />;
+  }
+}
+
+export default BoardDisplayContainer;
