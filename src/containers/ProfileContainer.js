8e45518b20356597d8e05a93d4d967274f00fbc6
Nick Paolino <github email address>
2017-12-31 14:55:24 -0500

    Added a bulletin board to the home page where a user will eventually be able to view their boards as sticky notes

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import withAuth from "../components/hocs/withAuth";
+import Board from "../components/board/Board";
+import "../home.css";
 
 class ProfileContainer extends Component {
+  state = {
+    notes: [
+      {
+        id: 5000,
+        subject: "Mozart",
+        left_position: "20px",
+        top_position: "0px"
+      }
+    ],
+    reset: false
+  };
+
+  handleDelete = () => {};
+
+  mapNotes = (notes, updatedBoard) => {
+    if (notes) {
+      this.setState({ notes: [...notes.notesList], reset: false });
+    }
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <div className="home">
+          <h3>Your Boards</h3>
+          <Board
+            notes={this.state.notes}
+            handleDelete={this.handleDelete}
+            mapNotes={this.mapNotes}
+          />
+          <div className="menu">
+            <button className="add" onClick={this.add}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
