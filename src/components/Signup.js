cae067c31eeae44efcf8ff1158d5a08192bcf947
Nick Paolino <github email address>
2018-01-01 14:51:48 -0500

    Updated signup to be a draggable element as well

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,6 +3,8 @@ import * as actions from "../actions/auth";
 import { connect } from "react-redux";
 import withAuth from "../components/hocs/withAuth";
 import { Form, Button, Input } from "semantic-ui-react";
+import Draggable from "react-draggable";
+import "../login.css";
 
 class Signup extends Component {
   constructor(props) {
@@ -35,29 +37,48 @@ class Signup extends Component {
     this.props.createUser(this.state, this.props.history);
   };
 
+  handleClick = e => {
+    e.target.focus();
+    e.target.select();
+  };
+
   render() {
     return (
-      <div>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Field inline>
-            <label>Username</label>
-            <Input
-              placeholder="Username"
-              name="username"
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Form.Field inline>
-            <label>Password</label>
-            <Input
-              placeholder="Password"
-              name="password"
-              type="password"
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Button>Sign Up</Button>
-        </Form>
+      <div className="boundary">
+        <Draggable bounds="parent">
+          <div className="login">
+            <div className="board">
+              <div className="prompt">
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Field inline>
+                    <h3>
+                      <label>Username</label>
+                    </h3>
+                    <Input
+                      placeholder="Username"
+                      name="username"
+                      onChange={this.handleChange}
+                      onClick={this.handleClick}
+                    />
+                  </Form.Field>
+                  <Form.Field inline>
+                    <h3>
+                      <label>Password</label>
+                    </h3>
+                    <Input
+                      placeholder="Password"
+                      name="password"
+                      type="password"
+                      onChange={this.handleChange}
+                      onClick={this.handleClick}
+                    />
+                  </Form.Field>
+                  <Button>Sign Up</Button>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </Draggable>
       </div>
     );
   }
